c6cca353973bc543d860676cf2cc15e0b941f21c
neeaoo <32387385+neeaoo@users.noreply.github.com>
2019-03-12 13:30:06 +0200

    2.versio

--- main.js
+++ main.js
@@ -23,12 +23,12 @@ function init(resultFromServer) {
 
     let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
     let temperatureElement = document.getElementById('temperature');
-    let humidityElement = document.getElementById('humidity');
+    //let minTempElement = document.getElementById('humidity');
     let windSpeedElement = document.getElementById('windSpeed');
     let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('documentIconImg');
+    //let weatherIcon = document.getElementById('documentIconImg'); *****
 
-    //weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+   //weatherIcon.src = 'http://openweathermap.org/img/w' + resultFromServer.weather[0].icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
@@ -36,7 +36,20 @@ function init(resultFromServer) {
     temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
     windSpeedElement.innerHTML ='Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
     cityHeader.innerHTML = resultFromServer.name;
-    //humidityElement.innerHTML = 'Humidity levels at ' + (resultFromServer.main.humidity) + '%';
+    //minTempElement.innerHTML = Math.floor(resultFromServer.main.temp_min) + '&#176';
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2})`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3};`
+    weatherContainer.style.visibility = 'visible';
+
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
